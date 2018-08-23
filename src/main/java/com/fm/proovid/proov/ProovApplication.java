d546e4df0f0fdca09b11b6ed075688258dfa6ddd
kristo.jurgenson <kristo.jurgenson@finestmedia.ee>
2018-08-23 14:05:01 +0300

    init

---
+++ src/main/java/com/fm/proovid/proov/ProovApplication.java
@@ -0,0 +1,12 @@
+package com.fm.proovid.proov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProovApplication.class, args);
+    }
+}
